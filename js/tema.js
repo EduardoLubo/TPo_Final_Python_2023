569be980cbb15afdc3e8347f75c0d239764d9dc7
EduardoLubo <89843311+EduardoLubo@users.noreply.github.com>
2023-07-04 23:08:28 -0300

    Add files via upload
    Se añade cambio de Tema y Local Storage

--- js/tema.js
+++ js/tema.js
@@ -1,3 +1,5 @@
+/*
+// Solo para la pagina
 document.addEventListener("DOMContentLoaded", function() {
   // Crea el elemento del botón
   var button = document.createElement("button");
@@ -18,4 +20,41 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById('tema-boton').innerHTML = 'Oscuro';
         }
    };
+});
+*/
+
+// Codigo guarda el dato en el LocalStorage 
+document.addEventListener("DOMContentLoaded", function() {
+  // Crea el elemento del boton
+  var button = document.createElement("button");
+  button.id = "tema-boton";
+  button.className = "tema-boton";
+  button.textContent = "Oscuro";
+  var divBoton = document.getElementById("miboton");
+  divBoton.appendChild(button);
+  
+  // Funcion para cambiar el tema
+  function cambiarTema() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var newTheme = currentTheme === "light" ? "dark" : "light";
+    
+    // Actualizar el atributo "data-theme" en el elemento HTML
+    document.documentElement.setAttribute("data-theme", newTheme);
+    
+    // Actualizar el texto del botón
+    document.getElementById("tema-boton").textContent = newTheme === "light" ? "Oscuro" : "Claro";
+    
+    // Guardar el tema seleccionado en el localStorage
+    localStorage.setItem("theme", newTheme);
+  }
+  
+  // Verificar si hay un tema guardado en el localStorage
+  var savedTheme = localStorage.getItem("theme");
+  if (savedTheme) {
+    document.documentElement.setAttribute("data-theme", savedTheme);
+    document.getElementById("tema-boton").textContent = savedTheme === "light" ? "Oscuro" : "Claro";
+  }
+  
+  // Asignar el evento click al botón
+  document.getElementById("tema-boton").onclick = cambiarTema;
 });
