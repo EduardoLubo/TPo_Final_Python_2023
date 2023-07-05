32ecedfd58fd6ff53a577772931322b8d491f874
EduardoLubo <89843311+EduardoLubo@users.noreply.github.com>
2023-07-04 22:34:11 -0300

    Update tema.js
    JS crea boton

--- js/tema.js
+++ js/tema.js
@@ -1,13 +1,21 @@
-const temaBoton = document.getElementById('tema-boton');
-const body = document.body;
-
-temaBoton.addEventListener('click', cambiarTema);
-
-function cambiarTema() {
-    body.classList.toggle('dark-theme');
-    if (body.classList.contains('dark-theme')) {
-        temaBoton.textContent = 'Claro';
-    } else {
-        temaBoton.textContent = 'Oscuro';
-    }
-}
+document.addEventListener("DOMContentLoaded", function() {
+  // Crea el elemento del botón
+  var button = document.createElement("button");
+  button.id = "tema-boton";
+  button.className = "tema-boton";
+  button.textContent = "Oscuro";
+  var divBoton = document.getElementById("miboton");
+  divBoton.appendChild(button);
+  
+  document.getElementById("tema-boton").onclick = function() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    if (currentTheme === null || currentTheme === "light"){ 
+        document.documentElement.setAttribute('data-theme', "dark");
+        document.getElementById('tema-boton').innerHTML = 'Claro';
+        }
+    else{
+        document.documentElement.setAttribute('data-theme', "light");
+        document.getElementById('tema-boton').innerHTML = 'Oscuro';
+        }
+   };
+});
